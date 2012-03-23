0b670aa32c5e2585e8eb35988d116427b55981c5
Peter Bilodeau <peterjbilodeau@gmail.com>
2012-03-23 12:57:59 -0400

    working motors code

--- quadcopter/i2c/i2c_slave.h
+++ quadcopter/i2c/i2c_slave.h
@@ -6,16 +6,20 @@
 
 // if the LSB of the slave address is set, slave will respond to general call 0x00 also
 #define SLAVEADDRESS 0xAA
+#define SLAVEBUFFERSIZE 20
 
 void setup_i2c_slave();
 void process_slave_receive();
 void process_slave_transmit();
 
+void process_code_received();
+
+char slave_command_ready = 0;
 char slave_receive_buffer_index;
-char slave_receive_buffer[20];
+char slave_receive_buffer[SLAVEBUFFERSIZE];
 
 char slave_transmit_buffer_index;
-char slave_transmit_buffer[20];
+char slave_transmit_buffer[SLAVEBUFFERSIZE];
 
 void setup_i2c_slave(){
 	TWAR = SLAVEADDRESS; // set this chip's own slave address
@@ -25,37 +29,43 @@ void setup_i2c_slave(){
 
 // interrupt handler for watching the TWINT flag
 ISR(TWI_vect){
-	TWSR |= (1<<TWINT); // have to set the bit to clear the flag
 	// read out from the status register
 	char status = TWSR & 0xF8; // mask off the prescaler bits
 	if (status == 0x60){
 		slave_receive_buffer_index = 0;
-	}else if(){
+	}else if(status == 0x80){
 		process_slave_receive();
-	}else if (status == 0xA8){
+	}else if(status == 0xA8){
+		slave_transmit_buffer_index = 0;
 		process_slave_transmit();
+	}else if(status == 0xB8){
+		process_slave_transmit();
+	}else if(status == 0xA0){ // STOP after receive  or repeated START
+		slave_command_ready = 1;
+	}else if (status == 0xC0){ // Transmit finished
+		//do nothing
 	}else{
 		// BAD STATUS CODE READ
 	}
+	TWCR |= (1<<TWINT); // trigger the next TWI action
 }
 
 void process_slave_receive(){
-	// switch to slave receiver mode
-	slave_receive_buffer_index = 0;
-	while((TWSR & 0xF8) != 0xA0){
-		while();
+	if(slave_receive_buffer_index > SLAVEBUFFERSIZE){
 		slave_receive_buffer[slave_receive_buffer_index] = TWDR;
 		slave_receive_buffer_index++;
+	}else{
+		//ERROR
 	}
-	
-	// first byte is the sensor code
-	// second byte is set or get
-	// third (optional) byte is the high byte of the value to set
-	// fourth (optional) byte is the low byte of the value to set
 }
 
 void process_slave_transmit(){
-	// switch to slave transmitter mode
+	if (slave_transmit_buffer_index < SLAVEBUFFERSIZE){
+		TWDR = slave_transmit_buffer[slave_transmit_buffer_index];
+		slave_transmit_buffer_index++;
+	}else{
+		//ERROR
+	}
 }
 
 #endif
--- quadcopter/motors/motors.c
+++ quadcopter/motors/motors.c
@@ -1,4 +1,6 @@
 #include <avr/io.h>
+#include "../lib/AVRserial.h"
+#include <stdio.h>
 
 #define NORTHMOTOR 0
 #define SOUTHMOTOR 1
@@ -11,33 +13,56 @@
 
 void setup_motors();
 void set_motor_power(char motor, char power);
+void forward_command();
 
 int main(){
-	setup_motors();
+        setup_serial();
+        setup_motors();
+	serial_command_ready = 0;
+        while(1){
+                if (serial_command_ready){
+                        forward_command();
+                        serial_command_ready = 0;
+                }   
+        }   
+        return 0;
+}
 
-	while(1){
-			set_motor_power(SOUTHMOTOR,10);
-	}	
-	return 0;
+void forward_command(){
+        if(!strcmp(receive_buffer,"IDLE")){
+		set_motor_power(SOUTHMOTOR,0);
+	}else if(!strcmp(receive_buffer,"FULL")){
+		set_motor_power(SOUTHMOTOR,100);
+	}else if(!strncmp(receive_buffer,"SET ",4)){
+		int power;
+		sscanf(receive_buffer+4,"%d",&power);
+		if ((power >= 0 )&&(power<=100))
+			set_motor_power(SOUTHMOTOR,(int)power);
+	}else if(!strcmp(receive_buffer,"MAX")){
+		OCR0B = 255;
+	}else if(!strcmp(receive_buffer,"OFF")){
+		OCR0B = 0;
+	}
 }
 
 void setup_motors(){
 // setup timer0 for north and south
-// use WGM mode 5, counts up and down from zero to OCRA
-	TCCR0A = 0xA1; // clear output pins on compare match
-	TCCR0B = (1<<WGM02)|(5); // use prescaler 1/1024 so the clock counts in units of 128 micro seconds
-
-// setup timer2 for east and west
-	TCCR2A = 0xA1; // clear output pins on compare match, counts up and down from zero to OCRA
-	TCCR2B = (1<<WGM02)|5;
+// use WGM mode 3, CTC with TOP = 255,
+	TCCR0A = 0xA3; // clear output pins on compare match
+	TCCR0B = (0<<WGM02)|4; // use prescaler 1/256
+		// clock overflows at like 122Hz
 	
-	// TOP value of FF leaves a frequency of ~30hz Hz	
+// setup timer2 for east and west
+	TCCR2A = 0xA3; // clear output pins on compare match, counts up and down from zero to OCRA
+	TCCR2B = (0<<WGM02)|4;
+	
+	// TOP value of FF leaves a frequency of ~122hz Hz	
 	// pwm frequency needs to be 50hz to 500hz
 		
-	OCR0A = 156; // yields a pulse on of about 1 millisecond
-	OCR0B = 8;
-	OCR2A = 156;
-	OCR2B = 8;
+	OCR0A = 32; // yields a pulse on of about 1 ms
+	OCR0B = 32; // 64 yields a pulse of just over 2 ms
+	OCR2A = 32;
+	OCR2B = 32;
 
 // enable output mode for all four pins	
 	DDRD |= (1<<6)|(1<<5)|(1<<3);
@@ -46,20 +71,19 @@ void setup_motors(){
 }
 
 // sets the percent power of the given motor
-// this timer setup only lets us use 1/8 resolution, this is BAD.
 void set_motor_power(char motor, char power){
 	switch(motor){
 		case NORTHMOTOR:
-			OCR0A = 8 + power/100.*8;
+			OCR0A = 32 + power/100.*32;
 			break;
 		case SOUTHMOTOR:
-			OCR0B = 8 + power/100.*8;
+			OCR0B = 32 + power/100.*32;
 			break;
 		case EASTMOTOR:
-			OCR2A = 8 + power/100.*8;
+			OCR2A = 32 + power/100.*32;
 			break;
 		case WESTMOTOR:
-			OCR2B = 8 + power/100.*8;
+			OCR2B = 32 + power/100.*32;
 			break;
 	}
 }
