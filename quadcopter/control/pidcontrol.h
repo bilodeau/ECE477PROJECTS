d324a49b86966e1b62a0d9bd5850304a4331c97a
mbbutler <m.brady.butler@gmail.com>
2012-04-09 14:52:49 -0300

    Update quadcopter/control/pidcontrol.h

--- quadcopter/control/pidcontrol.h
+++ quadcopter/control/pidcontrol.h
@@ -1,7 +1,5 @@
 #ifndef PIDCONTROL_H_
 #define PIDCONTROL_H_
-#include <stdlib.h>
-
 
 struct pid_values{
 int Kp; //constants for the pid controllers
@@ -26,21 +24,6 @@ void set_attrib(struct pid_values *s, int p, int i, int d,int limit) {
 	s->Kd = d;
 	s->old_error = 0;
 	gain_limit = limit;
-}
-
-int get_adj_roll(struct sensor_data *s) {
-	s->filt_roll_angle = (FILTER_C)*(s->filt_roll_angle + s->gyroscope.roll*DELTA_T) + (1 - FILTER_C)*(s->nunchuck_roll);
-	return s->filt_roll_angle;
-}
-
-int get_adj_pitch(struct sensor_data *s) {
-	s->filt_pitch_angle = (FILTER_C)*(s->filt_pitch_angle + s->gyroscope.pitch*DELTA_T) + (1 - FILTER_C)*(s->nunchuck_pitch);
-	return s->filt_pitch_angle;
-}
-
-int get_adj_alt(struct sensor_data *s) {
-	s->filt_alt = ALT_FILTER_C*(s->filt_alt) + (1 - ALT_FILTER_C)*(s->sonar_distance);
-	return s->filt_alt;
 }
 
 int get_gain(struct pid_values *s, int goal, int actual){
